6603d8f65a477706c35a7a04a6bece2e926ad236
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-08-12 12:36:32 +0200

    tilføj billeder

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,7 +7,7 @@ const projects = [
       " En webapp, der hjælper med at organisere hvedagen og holde styr på aktiviteter.",
     description: "Placeholder (ved ik hvad der skal stå endnu).",
     tags: ["React", "Supabase", "Animationsdesign"],
-    image: `${import.meta.env.BASE_URL}portfolio-placeholder.svg`,
+    image: `${import.meta.env.BASE_URL}hoomie-app.png`,
     links: [
       {
         label: "Live site",
@@ -27,7 +27,7 @@ const projects = [
       "En hjemmeside, der designet til specifik målgruppe, med fokus på idensitet og brugeroplevelse.",
     description: "Placeholder (ved ik hvad der skal stå endnu)",
     tags: ["Design", "Brugeroplevelse", "Proces"],
-    image: `${import.meta.env.BASE_URL}portfolio-placeholder.svg`,
+    image: `${import.meta.env.BASE_URL}wanted-archives.png`,
     links: [
       {
         label: "Live site",
@@ -47,7 +47,7 @@ const projects = [
       "Optimering af tidligere projekt, fokus på semantisk struktur, design og brugeroplevelse.",
     description: "Placeholder (ved ik hvad der skal stå endnu).",
     tags: ["Design", "Semantisk struktur", "Optimering"],
-    image: `${import.meta.env.BASE_URL}portfolio-placeholder.svg`,
+    image: `${import.meta.env.BASE_URL}akvarie-projekt.png`,
     links: [
       {
         label: "Live site",
