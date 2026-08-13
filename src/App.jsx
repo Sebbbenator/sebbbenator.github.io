c65ecd3e181b396565edbc58f88b7504ac253f1b
Sebbbenator <eaa25ssg@students.eaaa.dk>
2026-08-13 14:06:59 +0200

    footer indsat

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -31,11 +33,14 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
           <Route path="/projects/:slug" element={<ProjectPage />} />
           <Route path="/contact" element={<ContactPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
+
+      <Footer />
     </>
   );
 }
